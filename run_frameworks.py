4f777de0f64c83a8287d3cbd57f414e7d8ec2319
BoyuanJackChen <bc3194@nyu.edu>
2025-07-31 18:49:27 +0400

    Removed utils

--- run_frameworks.py
+++ run_frameworks.py
@@ -4,7 +4,7 @@ import argparse
 
 from tqdm import tqdm
 
-from utils import is_json_file, model_type
+# from utils import is_json_file, model_type
 from backbones import get_backbone
 from frameworks import get_framework
 
@@ -13,18 +13,14 @@ from frameworks import get_framework
 parser = argparse.ArgumentParser()
 parser.add_argument("--data-dir",      type=str,        help="PATH to the JSON file to conduct inference on.")
 parser.add_argument("--save-dir",      type=str,        help="PATH to the folder to save the results.", default="results/")
-
-parser.add_argument("--model",         type=model_type,        help="Name of the model.")
-
+parser.add_argument("--model",         type=str,        help="Name of the model.")
 parser.add_argument("--frameworks",    type=str,        help="List of framework names to employ.", nargs="+")
-
 parser.add_argument("--device",        type=int,        help="Device to run models on.", default=0)
 cmd_args = vars(parser.parse_args())
 
 # Load data
-assert is_json_file(cmd_args["data_dir"])
+# assert is_json_file(cmd_args["data_dir"])
 data = json.load(open(cmd_args["data_dir"], "r"))
-
 model = get_backbone(cmd_args["model"])
 
 for framework in tqdm(cmd_args["frameworks"], ascii=True, desc="Frameworks"):
